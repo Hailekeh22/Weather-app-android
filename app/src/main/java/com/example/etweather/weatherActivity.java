ddf99bf5d1f8392b4624557315f4f20fa0b9cd45
Hailekeh22 <hailekjt@gmail.com>
2024-10-05 20:47:16 +0300

    second activity

--- app/src/main/java/com/example/etweather/weatherActivity.java
+++ app/src/main/java/com/example/etweather/weatherActivity.java
@@ -1,2 +1,24 @@
-package com.example.etweather;public class weatherActivity {
+package com.example.etweather;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class weatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
